3aad6b0bbcf8bfd8f69b659e6b0f3a2ab2894ae2
IgnacioBockl <45101161+nachociruja96@users.noreply.github.com>
2020-10-10 17:55:58 -0300

    InjectionDependency - declaracion de clase, sin aplicacion

---
+++ ECommerceTessa/ECommerceTessa.Application.IoC/InjectionDependency.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ECommerceTessa.Application.IoC
+{
+    public class InjectionDependency
+    {
+        public static void ConfigurationServices(IServiceCollection service)
+        {
+            //////////////////// EJEMPLO ////////////////////
+            
+            //Scoped es cuando queremos servir la misma instancia dentro del mismo contexto de una peticion de HTTP
+            //Transient es cuando queremos servir una nueva instancia cada vez q aparesca como dependencia
+            //Singleton es cuando queremos servir la misma instancia siempre
+
+
+            //services.AddTransient<IProvinceRepository, ProvinceRepository>();
+
+
+            //version generica
+            //services.AddTransient(typeof(IRepository<>), typeof(Repository<>));
+            ////services.AddTransient<IRepository<Domain.Entities.Province>, Repository<Domain.Entities.Province>>();
+
+        }
+    }
+}
