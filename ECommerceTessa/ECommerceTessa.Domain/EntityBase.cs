0a0ed5e5e70e71f6a83dcbd0bdf4643fe7c83095
IgnacioBockl <45101161+nachociruja96@users.noreply.github.com>
2020-10-10 17:42:38 -0300

    Repository

---
+++ ECommerceTessa/ECommerceTessa.Domain/EntityBase.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace ECommerceTessa.Domain
+{
+    public class EntityBase
+    {
+        [Key]
+        public long Id { get; set; }
+
+        [Required]
+        public bool ErasedState { get; set; }
+    }
+}
