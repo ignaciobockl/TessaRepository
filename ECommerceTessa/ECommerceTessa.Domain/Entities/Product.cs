8429c58ec73ca87bee298035c559e6ac6b0cf04b
IgnacioBockl <45101161+nachociruja96@users.noreply.github.com>
2020-10-16 15:17:48 -0300

    Entity Person (Incomplete)

---
+++ ECommerceTessa/ECommerceTessa.Domain/Entities/Product.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ECommerceTessa.Domain.Entities
+{
+    public class Product : EntityBase
+    {
+        public string Code { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public string Photo { get; set; }
+
+        public bool DiscountStock { get; set; }
+
+        public bool Discontinued { get; set; }
+
+        public bool ShowBrand { get; set; }
+
+        public bool Slow { get; set; }
+
+        public long BrandId { get; set; }
+
+        public long CategoryId { get; set; }
+    }
+}
