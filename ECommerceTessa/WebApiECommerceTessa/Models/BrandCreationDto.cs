0bd85db97533fd40f0bbe74c9c6bf72f7c4fcc00
IgnacioBockl <45101161+nachociruja96@users.noreply.github.com>
2020-10-20 23:17:37 -0300

    BrandController (GetById, GetAll, Create, Update, Delete)

---
+++ ECommerceTessa/WebApiECommerceTessa/Models/BrandCreationDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ECommerceTessa.Service.Interface.Brand.DTOs;
+
+namespace WebApiECommerceTessa.Models
+{
+    public class BrandCreationDto : BrandDto
+    {
+    }
+}
